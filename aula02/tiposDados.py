dfe6181d82317c182ba7e4266c9d4428ed644a42
Samuel <samukactto@gmail.com>
2023-11-12 12:56:45 -0300

    tipos de dados

---
+++ aula02/tiposDados.py
@@ -0,0 +1,11 @@
+#tipos de dados em python
+#Texto = str | Númerico = int, float, complex |
+#Sequência = list, tuple, range | Mapa = dict |
+#Coleção =  set, fronzenset | Booleano = bool |
+#Binário = bytes, bytearray, memoryview.
+
+print(1 + 10)
+print(2.2 + 1 + .8)
+print(True)
+print(False)
+print("python")
