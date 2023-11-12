cc125bd3ba87270ec2729c79df60d2ecd076d7a4
Samuel <samukactto@gmail.com>
2023-11-12 13:08:03 -0300

    aula04

---
+++ convTipos.py
@@ -0,0 +1,12 @@
+print(int(3.14))
+print(int("16"))
+print(float("16.9"))
+print(float(100))
+
+valor = 10
+valorStr = str(valor)
+print(type(valor))
+print(type(valorStr))
+
+print(100 / 2)
+print(100 // 2)
