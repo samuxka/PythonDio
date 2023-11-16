1e6e211ff0b6b5fa1b2fde98fc58f44fdbbc5e4e
Samuel <samukactto@gmail.com>
2023-11-15 22:11:54 -0300

    dig

---
+++ aula11/dicionaros.py
@@ -0,0 +1,17 @@
+pessoa = {"nome": "samuel", "idade": 16}
+
+pessoa = dict(nome = "samuel", idade = 16)
+
+pessoa["telefone"] = "75988098306"
+
+#captura de dados
+
+dados["nome"]
+dados["idade"]
+dados["telefone"]
+
+dados["nome"] = "maria"
+
+dados["idade"] = 21
+
+dados["telefone"]="75988098306"
